54f9d440f819f953baf5d7e3e5276205dfd8a1f0
Shadocks <mickael.bardeau@laposte.net>
2018-05-02 14:28:18 +0200

    First commit in agreement with cache issue

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\CacheKernel;
 use App\Kernel;
 use Symfony\Component\Debug\Debug;
 use Symfony\Component\Dotenv\Dotenv;
@@ -33,6 +34,7 @@ if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
 }
 
 $kernel = new Kernel($env, $debug);
+$kernel = new CacheKernel($kernel);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
